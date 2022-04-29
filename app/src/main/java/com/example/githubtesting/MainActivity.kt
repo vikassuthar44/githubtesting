b8adf0b137234b4ca94b208c82f7b5560a1f409e
vikassuthar <pocha.ram@byjus.com>
2022-04-29 10:24:57 +0530

    add method

--- app/src/main/java/com/example/githubtesting/MainActivity.kt
+++ app/src/main/java/com/example/githubtesting/MainActivity.kt
@@ -7,5 +7,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        printMessage()
+    }
+
+
+    private fun printMessage() {
+        println("Hello World")
     }
 }
